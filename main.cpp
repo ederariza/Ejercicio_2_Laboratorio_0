26853339aea9cfad1f282e26b027e679cd29eddb
Infromatica II <eder.ariza@udea.edu.co>
2020-09-23 00:11:58 -0500

    Ejercicio 1 Laboratorio 0

--- main.cpp
+++ main.cpp
@@ -5,26 +5,26 @@ int  i, j, k, N;
 char letra;
 int main(void){
 
-    cout<< "Ingrese el numero del tamano del triangulo mariposa: ";
+    cout<< "Ingrese el numero de dimension del triangulo mariposa: ";
     cin>> N;
 
     for(i=1; i<=N; i++){
         letra = 65;
 
         for(j=1; j<=i; j++){
-            cout<< letra;
+            cout<< letra << " ";
             letra++;
         }
 
         for(j=0; j<((N-i)*2-1); j++){
-            cout<<" ";
+            cout<<"  ";
         }
 
         for(j=1; j<=i; j++){
             letra--;
 
             if(letra != (65+N-1)){
-                cout<<letra;
+                cout<<letra << " ";
             }
         }
 
@@ -35,19 +35,19 @@ int main(void){
         letra = 65;
 
         for(j=i; j<N; j++){
-            cout<< letra;
+            cout<< letra << " ";
             letra++;
         }
 
         for(j=0; j<=k; j++){
-            cout<< " ";
+            cout<< "  ";
         }
 
         k+=2;
 
         for(j=i; j<N; j++){
             letra--;
-            cout<< letra;
+            cout<< letra << " ";
         }
 
         cout<< endl;
