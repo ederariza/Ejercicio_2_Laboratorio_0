6ca73df74862dd7e0cc1007d929be90707de7dff
Infromatica II <eder.ariza@udea.edu.co>
2020-10-05 23:10:29 -0500

    Ejercicio 2 Laboratorio 0

--- main.cpp
+++ main.cpp
@@ -24,53 +24,54 @@ int  i, j, k, N;
 char letra;
 int main(void){
 
-    cout<< "Ingrese el numero de dimension del triangulo mariposa: ";
-    cin>> N;
+    cout << "Ingrese el numero de dimension del triangulo mariposa: ";
+    cin >> N;
 
-    for(i=1; i<=N; i++){
+    for (i = 1; i <= N; i++){
         letra = 65;
 
-        for(j=1; j<=i; j++){
-            cout<< letra << " ";
+        for (j = 1; j <= i; j++){
+            cout << letra << " ";
             letra++;
         }
 
-        for(j=0; j<((N-i)*2-1); j++){
-            cout<<"  ";
+        for (j = 0; j < ((N-i)*2-1); j++){
+            cout << "  ";
         }
 
-        for(j=1; j<=i; j++){
+        for (j = 1; j <= i; j++){
             letra--;
 
-            if(letra != (65+N-1)){
-                cout<<letra << " ";
+            if (letra != (65+N-1)){
+                cout << letra << " ";
             }
         }
 
-        cout<< endl;
+        cout << endl;
     }
 
-    for(i=1; i<N; i++){
+    for (i = 1; i < N; i++){
         letra = 65;
 
-        for(j=i; j<N; j++){
-            cout<< letra << " ";
+        for (j = i; j < N; j++){
+            cout << letra << " ";
             letra++;
         }
 
-        for(j=0; j<=k; j++){
-            cout<< "  ";
+        for (j = 0; j <= k; j++){
+            cout << "  ";
         }
 
-        k+=2;
+        k += 2;
 
-        for(j=i; j<N; j++){
+        for (j = i; j < N; j++){
             letra--;
-            cout<< letra << " ";
+            cout << letra << " ";
         }
 
-        cout<< endl;
+        cout << endl;
 
     }
+
 return 0;
 }
